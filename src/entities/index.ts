ba5c0bd0b1d3390a8adeea43272755ac0b5262b2
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-02-16 11:36:30 +0100

    test fix 19

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -9,7 +9,7 @@ export default async () => {
   const options: ConnectionOptions = {
     type: 'postgres',
     url: new Env().databaseURL,
-    ssl: process.env.NODE_ENV === 'testing-in-ci' ? false : { rejectUnauthorized: false },
+    ssl: { rejectUnauthorized: false },
     synchronize: true,
     dropSchema: process.env.NODE_ENV === 'development',
     entities: [UserEntity],
