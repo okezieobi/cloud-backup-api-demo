2412f4b47abd6a72ca36dc2e63d5ff193671c899
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-02-20 17:23:55 +0100

    test fix 3

--- src/tests/file.test.ts
+++ src/tests/file.test.ts
@@ -1,38 +1,40 @@
-// import { fileRepository, userRepository } from '../entities';
-// import FileServices, { SaveFileParams } from '../services/File';
+import { fileRepository, userRepository } from '../entities';
+import FileServices, { SaveFileParams } from '../services/File';
 
-// describe('File management tests', () => {
-//   const user = {
-//     name: 'test-username',
-//     email: 'test@email.com',
-//     password: 'test-password',
-//   };
+describe('File management tests', () => {
+  const user = {
+    name: 'test-username',
+    email: 'test-flie@email.com',
+    password: 'test-password',
+  };
 
-//   const file: SaveFileParams = {
-//     info: [
-//       { test: 'test' },
-//     ],
-//     user: {},
-//   };
+  const file: SaveFileParams = {
+    info: [
+      { test: 'test' },
+    ],
+    user: {},
+  };
 
-//   beforeAll(async () => {
-//     const userRepo = await userRepository();
-//     const fileRepo = await fileRepository();
+  beforeAll(async () => {
+    const userRepo = await userRepository();
+    const fileRepo = await fileRepository();
 
-//     await fileRepo.delete({});
-//     await userRepo.delete({});
+    await fileRepo.delete({});
 
-//     const registeredUser = userRepo.create(user);
-//     await userRepo.save(registeredUser);
+    const registeredUser = userRepo.create(user);
+    await userRepo.save(registeredUser);
 
-//     file.user = registeredUser;
-//     const file;
-//     await fileRepo.save();
-//   });
+    file.user = registeredUser;
+    const savedFile = fileRepo.create(file);
+    await fileRepo.save(savedFile);
+  });
 
-//   describe('Testing new file saving on database', () => {
-//     it('Saves a new file on a database', async () => {
-//       const { saveOne } = new FileServices();
-//     });
-//   });
-// });
+  describe('Testing new file saving on database', () => {
+    it('Saves a new file on a database', async () => {
+      const { saveOne } = new FileServices();
+      const { message } = await saveOne(file);
+      expect(message).toBeString();
+      expect(message).toEqual('New file successfully saved');
+    });
+  });
+});
